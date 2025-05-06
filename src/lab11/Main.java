7abb4a4ce5619f10e03719a1bc8258801d686e72
potun <potun3000@yahoo.com>
2025-05-05 22:09:06 -0700

    Created file

---
+++ src/lab11/Main.java
@@ -0,0 +1,36 @@
+package lab11;
+// TODO: Step 2 - Import file input statements here
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //TEST FOR STEP 1 (you can delete/comment this out after completing step 1)
+        Plant test = new Plant("Stinging Nettle,65,anti-inflammatory and culinary");
+        System.out.println("test plant:\n" + test + "\n\n");
+
+
+        //TODO: Step 2 - Declare + initialize variables for file input here
+
+
+        //TODO: Step 2 - Connect input stream to file (dont forget the try/catch!)
+
+
+        //TODO: Step 2 - create loop to read through whole file
+
+
+        //TODO: Step 3 - build Plant Objects and store into ArrayList
+
+
+
+        //TODO: Step 2 - close the input stream
+
+
+        //TODO: Step 3 - print contents of ArrayList
+
+    }
+}
