c01a38b835c491b31ef3b6625dfc156055e0dbf3
potun <potun3000@yahoo.com>
2025-11-16 16:14:22 -0800

    Updated file

--- src/lab10/Supplies.java
+++ src/lab10/Supplies.java
@@ -2,41 +2,41 @@ package lab10;
 
 public class Supplies {
 
-    // Instance variables
-    private String name;
-    private int quantity;
+	// Instance variables
+	private String name;
+	private int quantity;
 
-    // Default constructor
-    public Supplies() {
-        this.name = "Thing";
-        this.quantity = 0;
-    }
+	// Default constructor
+	public Supplies() {
+		this.name = "Thing";
+		this.quantity = 0;
+	}
 
-    // Full constructor
-    public Supplies(String name, int quantity) {
-        this.setName(name);
-        this.setQuantity(quantity);
-    }
+	// Full constructor
+	public Supplies(String name, int quantity) {
+		this.setName(name);
+		this.setQuantity(quantity);
+	}
 
-    // Getters and setters
-    public void setName(String name) {
-        this.name = name;
-    }
+	// Getters and setters
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setQuantity(int qty) {
-        this.quantity = qty;
-    }
+	public void setQuantity(int qty) {
+		this.quantity = qty;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getQuantity() {
-        return quantity;
-    }
+	public int getQuantity() {
+		return quantity;
+	}
 
-    // toString method
-    public String toString() {
-        return "SUPPLIES: " + this.name + ", QTY: " + quantity;
-    }
+	// toString method
+	public String toString() {
+		return "SUPPLIES: " + this.name + ", QTY: " + quantity;
+	}
 }
